21e7a9cf5edb4d51e833326c1c2f29361c37d9ee
Becket Harvey <becketfc@gmail.com>
2021-08-09 15:23:18 -0700

    added gallons to liters and ounces to gram

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,15 @@ function subtract(number1,number2){
 function fartocel(number1) {
   return (number1 - 32)* 5/9;
 }
+
+function galtolit(number1) {
+  return (number1 * 3.785);
+}
+
+function ouncetogram(number1) {
+  return (number1 * 28.35);
+}
+
 const number1 = parseInt(prompt("Enter degrees in farenheit:"));
 
 const result = fartocel(number1).toFixed(2) + " degrees Celsius";
