fc3a2f14e86186df9522090a0a9233b2ab72cc63
Becket Harvey <becketfc@gmail.com>
2021-08-11 15:36:24 -0700

    calculator will now work

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,28 @@
+
 function add(number1, number2) {
-	return number1 + number2;
+  return number1 + number2;
 }
 
-function divide(number1,number2){
-  return number1 / number2;
-}
-
-function multiply(number1,number2){
-  return number1 * number2;
-}
-
-function subtract(number1,number2){
+function subtract(number1, number2) {
   return number1 - number2;
 }
 
-function fartocel(number1) {
-  return (number1 - 32)* 5/9;
+function multiply(number1, number2) {
+  return number1 * number2;
 }
 
-function galtolit(number1) {
-  return (number1 * 3.785);
+function divide(number1, number2) {
+  return number1 / number2;
 }
 
-function ouncetogram(number1) {
-  return (number1 * 28.35);
-}
+
 
 $(document).ready(function() {
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-
-  alert(add(number1, number2));
-alert(result);
-
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
